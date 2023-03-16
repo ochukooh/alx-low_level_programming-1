fcb02466d39a8499ee6a099101942073040ac98c
malak al ammari <alammarimalak17@gmail.com>
2023-03-16 14:59:16 -0700

    diff size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ long long int b;
 float f;
 
 printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %d lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long int:  %d byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long long int: %ld byte(s)\n", (unsinged long)sizeof(b));
+printf("Size of a long long int: %d byte(s)\n", (unsinged long)sizeof(b));
 printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 
