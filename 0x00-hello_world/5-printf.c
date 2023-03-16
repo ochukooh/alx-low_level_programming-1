c88bbb59c78e8370c5fb7b82e142d784e0bd033b
malak al ammari <alammarimalak17@gmail.com>
2023-03-16 05:45:31 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-2nd main function
+ *
+ * Return: 0 when complete
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
