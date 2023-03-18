83ddc6cced64acece8580f617e310f690bbc039f
malak al ammari <alammarimalak17@gmail.com>
2023-03-17 17:22:15 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,14 @@
 int main(void)
 {
 int i;
-for (i = 0 ; i < 10 ; i++){
-if (i == 9)
+for (i = 0 ; i < 10 ; i++)
 {
-putchar(i + '0');
-}
-else
-putchar(i + '0');
+putchar(n + '0');
+if (n != 9)
+{
 putchar(',');
 putchar(' ');
 }
+}
 putchar('\n');
 return (0);
-}
